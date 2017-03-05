5fcd14a137ff48dc5a012db349a4521d899b44d1
admin <admin@dhcp-18-111-48-202.dyn.mit.edu>
2017-03-05 13:05:46 -0500

    alc calc is doing good

--- 2Drunk2Drive/ViewController.swift
+++ 2Drunk2Drive/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 
-
-
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     
+    
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var pick1: UIPickerView!
+    
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var pick2: UIPickerView!
+    
+    
+    @IBOutlet weak var text3: UITextField!
+    @IBOutlet weak var pick3: UIPickerView!
+    
+    
+    @IBOutlet weak var text4: UITextField!
+    @IBOutlet weak var pick4: UIPickerView!
 
+    var vodkaamount = ["1","2","3","4","5","6","7","8","9","10"]
+    var cocktailamount = ["1","2","3","4","5","6","7","8","9","10"]
+    var beeramount = ["1","2","3","4","5","6","7","8","9","10"]
+    var wineamount = ["1","2","3","4","5","6","7","8","9","10"]
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var countrows: Int = vodkaamount.count
+        if pickerView == pick2 {
+            countrows = self.cocktailamount.count
+        }
+        return countrows
+    }
+    
+    func pickerVi
+    
+    
+    
     @IBOutlet weak var Button: UIButton!
     
     @IBOutlet weak var Label: UILabel!
